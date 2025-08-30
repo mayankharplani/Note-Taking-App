9ef20521ae6c60ccb3682531549320072bd77b4d
Mayank Harplani <harplanimayank5@gmail.com>
2025-08-30 19:14:24 +0530

    deployment

--- backend/src/app.js
+++ backend/src/app.js
@@ -14,7 +14,7 @@ app.use(express.json())
 app.use(express.urlencoded({extended:true}))
 app.use(cookieParser())
 app.use(cors({
-     origin: process.env.FRONTEND_URL,
+     origin:[ process.env.FRONTEND_URL , "http://localhost:5173"],
      credentials: true
 }));
 
